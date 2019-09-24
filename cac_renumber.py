5fe06a80ee06b0ae179e2393081bd5a15ab68da1
Kilian Evang <noreply@texttheater.net>
2019-09-24 17:26:55 +0200

    Bugfix: syntax error

--- cac_renumber.py
+++ cac_renumber.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     start_pattern = re.compile(r'^ccg\(\d+,')
     next_number = 1
     for block in util.blocks(sys.stdin):
-        print start_pattern.sub('ccg({},'.format(next_number), block, 1),
+        print(start_pattern.sub('ccg({},'.format(next_number), block, 1), end='')
         next_number += 1
