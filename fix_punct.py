55278447d428a92e4c5039e32cc250fa2f2be96a
Kilian Evang <noreply@texttheater.net>
2020-10-15 09:27:15 +0200

    Bugfix: don't attach puncutation to itself

--- fix_punct.py
+++ fix_punct.py
@@ -7,6 +7,8 @@ import util
 
 def fix_punct(lines):
     (root_number,) = [i for i, l in enumerate(lines, start=1) if l != '' and l.split()[2] == '0']
+    if root_number == len(lines):
+        return # don't attach punctuation to itself
     fields = lines[-1].split()
     if fields[1] in ('.', '?', '!'):
         fields[2] = str(root_number)
