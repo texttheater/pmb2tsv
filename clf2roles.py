80dda0e563a165454ccd8b02a0ecea3d7459fab7
Kilian Evang <noreply@texttheater.net>
2020-11-20 16:04:29 +0100

    Filter Time roles based on semtags

--- clf2roles.py
+++ clf2roles.py
@@ -11,31 +11,30 @@ import sys
 
 if __name__ == '__main__':
     try:
-        _, clf_path, lemma_path = sys.argv
+        _, clf_path, lemma_path, semtag_path = sys.argv
     except ValueError:
-        print('USAGE: python3 clf2roles.py myfile.drs.clf myfile.lemma', file=sys.stderr)
+        print('USAGE: python3 clf2roles.py myfile.drs.clf myfile.lemma myfile.sem', file=sys.stderr)
         sys.exit(1)
-    with open(clf_path) as clf_file, open(lemma_path) as lemma_file:
-        clf_data = clf.read(clf_file)
-        lemma_data = (tuple(l.strip() for l in b) for b in blocks.read(lemma_file))
-        for words, fragments, symbols in clf.read_sentences(clf_file, lemma_data):
+    with open(clf_path) as clf_file, open(lemma_path) as lemma_file, open(semtag_path) as semtag_file:
+        for words, fragments, symbols, semtags in clf.read_sentences(clf_file, lemma_file, semtag_file):
             # change representation of constant arguments
             fragments = constants.add_constant_clauses(symbols, fragments)
             fragments = constants.replace_constants(fragments)
             # map referents to the number of the token that introduces them
             ref_toknum_map = {c[2]: i for i, f in enumerate(fragments, start=1) for c in f if c[1] in ('REF', 'Name') or drs.is_constant(c[1])}
-            # make a flat list of all clauses
-            clauses = [c for f in fragments for c in f]
             # extract (verbal) events
-            events = set(c[3] for c in clauses if c[2].startswith('"v.'))
+            events = set(c[3] for f in fragments for c in f if c[2].startswith('"v.'))
             # find predicate and argument token numbers for each event
             pred_arg_role_map = collections.defaultdict(dict)
-            for c in clauses:
-                if len(c) == 4 and c[2] in events and not drs.is_constant(c[3]):
-                    pred_toknum = ref_toknum_map[c[2]]
-                    arg_toknum = ref_toknum_map[c[3]]
-                    role = c[1]
-                    pred_arg_role_map[pred_toknum][arg_toknum] = role
+            for word, semtag, fragment in zip(words, semtags, fragments):
+                if semtag in ('NOW', 'PST', 'FUT', 'PRG', 'PFT'):
+                    continue
+                for c in fragment:
+                    if len(c) == 4 and c[2] in events and not drs.is_constant(c[3]):
+                        pred_toknum = ref_toknum_map[c[2]]
+                        arg_toknum = ref_toknum_map[c[3]]
+                        role = c[1]
+                        pred_arg_role_map[pred_toknum][arg_toknum] = role
             pred_toknums = tuple(pred_arg_role_map.keys())
             # output (one column per predicate)
             for toknum, word in enumerate(words, start=1):
